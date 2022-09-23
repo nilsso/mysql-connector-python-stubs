abdc1b9364f9ef65c30cff1f0ca2334236e58046
Nils Olsson <nilso@enosis.net>
2022-09-22 23:31:27 -0700

    Add errors to mysql.connection

--- mysql-stubs/connector/__init__.pyi
+++ mysql-stubs/connector/__init__.pyi
@@ -15,3 +15,48 @@ def connect(*args: typing.Any, **kwargs: typing.Any) -> MySQLConnection:
     Returns MySQLConnection or PooledMySQLConnection.
     """
     ...
+
+
+class Error(Exception):
+    """Exception that is base class for all other error exceptions"""
+    ...
+
+
+class Warning(Exception):
+    """Exception for important warnings"""
+
+
+class InterfaceError(Error):
+    """Exception for errors related to the interface"""
+
+
+class DatabaseError(Error):
+    """Exception for errors related to the database"""
+
+
+class InternalError(DatabaseError):
+    """Exception for errors internal database errors"""
+
+
+class OperationalError(DatabaseError):
+    """Exception for errors related to the database's operation"""
+
+
+class ProgrammingError(DatabaseError):
+    """Exception for errors programming errors"""
+
+
+class IntegrityError(DatabaseError):
+    """Exception for errors regarding relational integrity"""
+
+
+class DataError(DatabaseError):
+    """Exception for errors reporting problems with processed data"""
+
+
+class NotSupportedError(DatabaseError):
+    """Exception for errors when an unsupported database feature was used"""
+
+
+class PoolError(Error):
+    """Exception for errors relating to connection pooling"""
